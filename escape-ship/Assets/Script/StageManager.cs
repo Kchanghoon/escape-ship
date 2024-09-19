73c56b1397465901ae99ab11aef62a36009afd63
Kchanghoon <dkskspd2@naver.com>
2024-09-19 19:32:44 +0900

    chore : 시작시 플레이어 스타팅 위치 추가
    플레이어 스타트 위치의 다름을 해결하기 위해 해당 기능 추가.

--- escape-ship/Assets/Script/StageManager.cs
+++ escape-ship/Assets/Script/StageManager.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
-
-    void Start()
-    {
-        ActivateStage(0);  // 기본으로 Stage1을 활성화
-    }
     // 스테이지를 저장할 배열
     public GameObject[] stages;
+
+    // 플레이어를 저장할 변수
+    public GameObject player;
+
+    // 각 스테이지별 플레이어 시작 위치
+    public Transform[] playerStartPoints;
 
     // 특정 스테이지를 활성화하는 함수
     public void ActivateStage(int stageIndex)
@@ -23,6 +24,10 @@ public class StageManager : MonoBehaviour
         if (stageIndex >= 0 && stageIndex < stages.Length)
         {
             stages[stageIndex].SetActive(true);
+
+            // 플레이어를 해당 스테이지의 시작 위치로 이동
+            player.transform.position = playerStartPoints[stageIndex].position;
+            player.transform.rotation = playerStartPoints[stageIndex].rotation;
         }
     }
 }
